6d47f533dfbfa1727c95ac12680261a1bcd95aeb
Nomandiu <115807603+Nomandiu@users.noreply.github.com>
2023-09-11 19:11:49 +0600

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,6 @@
+#By Noman_99, contest: Codeforces Beta Round 4 (Div. 2 Only), problem: (A) Watermelon, Accepted, #, Copy
+w=int(input())
+if (w%2==0 and w!=2):
+   print("YES")
+else:
+    print("NO")
